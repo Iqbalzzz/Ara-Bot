1ca7f02572f47100b7a88da436254cbd7975fb39
Iqbalzzz <86013471+Iqbalzzz@users.noreply.github.com>
2021-07-22 21:51:22 +0700

    Add files via upload

---
+++ message/Ara.js
@@ -0,0 +1,325 @@
+"use strict";
+const {
+    WAConnection,
+    MessageType,
+    Presence,
+    Mimetype,
+    GroupSettingChange,
+    MessageOptions,
+    WALocationMessage,
+    WA_MESSAGE_STUB_TYPES,
+    ReconnectMode,
+    ProxyAgent,
+    waChatKey,
+    mentionedJid,
+    WA_DEFAULT_EPHEMERAL
+} = require("@adiwajshing/baileys");
+const fs = require("fs");
+const moment = require("moment-timezone");
+const { exec, spawn } = require("child_process");
+const ffmpeg = require("fluent-ffmpeg");
+const fetch = require("node-fetch");
+const axios = require("axios");
+const speed = require("performance-now");
+
+const { color, bgcolor } = require("../lib/color");
+const { getBuffer, getRandom, getGroupAdmins, runtime } = require("../lib/myfunc");
+const { isLimit, limitAdd, getLimit, giveLimit, addBalance, kurangBalance, getBalance, isGame, gameAdd, givegame, cekGLimit } = require("../lib/limit");
+const _prem = require("../lib/premium");
+const afk = require("../lib/afk");
+const { addBanned, unBanned, BannedExpired, cekBannedUser } = require("../lib/banned");
+const { isTicTacToe, getPosTic } = require("../lib/tictactoe");
+const tictac = require("../lib/tictac");
+const { yta, ytv } = require("../lib/ytdl");
+const { getUser, getPost, searchUser } = require('../lib/instagram');
+const { fbdl } = require("../lib/fbdl");
+const { fakeStatus, fakeToko } = require("./fakeReply");
+const game = require("../lib/game");
+const { addBadword, delBadword, isKasar, addCountKasar, isCountKasar, delCountKasar } = require("../lib/badword");
+
+let pendaftar = JSON.parse(fs.readFileSync('./database/user.json'))
+let setting = JSON.parse(fs.readFileSync('./config.json'));
+let mess = JSON.parse(fs.readFileSync('./message/mess.json'));
+let limit = JSON.parse(fs.readFileSync('./database/limit.json'));
+let glimit = JSON.parse(fs.readFileSync('./database/glimit.json'));
+let balance = JSON.parse(fs.readFileSync('./database/balance.json'));
+let premium = JSON.parse(fs.readFileSync('./database/premium.json'));
+let ban = JSON.parse(fs.readFileSync('./database/ban.json'));
+let antilink = JSON.parse(fs.readFileSync('./database/antilink.json'));
+let badword = JSON.parse(fs.readFileSync('./database/badword.json'));
+let grupbadword = JSON.parse(fs.readFileSync('./database/grupbadword.json'));
+let senbadword = JSON.parse(fs.readFileSync('./database/senbadword.json'));
+let mute = JSON.parse(fs.readFileSync('./database/mute.json'));
+
+// Game
+let tictactoe = [];
+let tebakgambar = [];
+let family100 = [];
+
+// Prefix
+let multi = true
+let nopref = false
+let prefa = 'anjing'
+
+// Mode
+let mode = 'public'
+
+let {
+    ownerNumber,
+    limitCount,
+    lolkey,
+    gamewaktu
+} = setting
+
+moment.tz.setDefault("Asia/Jakarta").locale("id");
+
+module.exports = async(iqbl, msg, blocked, baterai, _afk, welcome, left) => {
+    try {
+        const { menu } = require("./help");
+        const { type, quotedMsg, isGroup, isQuotedMsg, mentioned, sender, from, fromMe, pushname, chats, isBaileys } = msg
+        if (isBaileys) return
+        const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+        const args = chats.split(' ')
+		const command = chats.toLowerCase().split(' ')[0] || ''
+
+        if (multi){
+		    var prefix = /^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/.test(command) ? command.match(/^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/gi) : '#'
+        } else {
+            if (nopref){
+                prefix = ''
+            } else {
+                prefix = prefa
+            }
+        }
+
+        const isCmd = command.startsWith(prefix)
+        const q = chats.slice(command.length + 1, chats.length)
+        const body = chats.startsWith(prefix) ? chats : ''
+
+        const botNumber = iqbl.user.jid
+        const groupMetadata = isGroup ? await iqbl.groupMetadata(from) : ''
+		const groupName = isGroup ? groupMetadata.subject : ''
+		const groupId = isGroup ? groupMetadata.jid : ''
+		const groupMembers = isGroup ? groupMetadata.participants : ''
+		const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+		const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
+		const isGroupAdmins = groupAdmins.includes(sender) || false
+
+        const isOwner = ownerNumber.includes(sender)
+        const isPremium = isOwner ? true : _prem.checkPremiumUser(sender, premium)
+	    const isBan = cekBannedUser(sender, ban)
+        
+        const isAntiLink = isGroup ? antilink.includes(from) : false
+        const isWelcome = isGroup ? welcome.includes(from) : false
+        const isLeft = isGroup ? left.includes(from) : false
+        const isUser = pendaftar.includes(sender)
+        const isBadword = isGroup ? grupbadword.includes(from) : false
+        const isMuted = isGroup ? mute.includes(from) : false
+        
+        const gcounti = setting.gcount
+        const gcount = isPremium ? gcounti.prem : gcounti.user
+
+        const isUrl = (url) => {
+            return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
+        }
+        function monospace(string) {
+            return '```' + string + '```'
+        }   
+        function jsonformat(string) {
+            return JSON.stringify(string, null, 2)
+        }
+        function randomNomor(angka){
+            return Math.floor(Math.random() * angka) + 1
+        }
+        const nebal = (angka) => {
+            return Math.floor(angka)
+        }
+        const reply = (teks) => {
+            return iqbl.sendMessage(from, teks, text, {quoted:msg})
+        }
+        const sendMess = (hehe, teks) => {
+            return iqbl.sendMessage(hehe, teks, text)
+        }
+        const mentions = (teks, memberr, id) => {
+            let ai = (id == null || id == undefined || id == false) ? iqbl.sendMessage(from, teks.trim(), extendedText, {contextInfo: {"mentionedJid": memberr}}) : iqbl.sendMessage(from, teks.trim(), extendedText, {quoted: msg, contextInfo: {"mentionedJid": memberr}})
+            return ai
+        }
+        async function sendFileFromUrl(from, url, caption, msg, men) {
+            let mime = '';
+            let res = await axios.head(url)
+            mime = res.headers['content-type']
+            let type = mime.split("/")[0]+"Message"
+            if(mime === "image/gif"){
+                type = MessageType.video
+                mime = Mimetype.gif
+            }
+            if(mime === "application/pdf"){
+                type = MessageType.document
+                mime = Mimetype.pdf
+            }
+            if(mime.split("/")[0] === "audio"){
+                mime = Mimetype.mp4Audio
+            }
+            return iqbl.sendMessage(from, await getBuffer(url), type, {caption: caption, quoted: msg, mimetype: mime, contextInfo: {"mentionedJid": men ? men : []}})
+        }
+        const textImg = (teks) => {
+            return iqbl.sendMessage(from, teks, text, {quoted: msg, thumbnail: fs.readFileSync(setting.pathImg)})
+        }
+
+        const isImage = (type === 'imageMessage')
+        const isVideo = (type === 'videoMessage')
+        const isSticker = (type == 'stickerMessage')
+        const isQuotedImage = isQuotedMsg ? (quotedMsg.type === 'imageMessage') ? true : false : false
+        const isQuotedVideo = isQuotedMsg ? (quotedMsg.type === 'videoMessage') ? true : false : false
+        const isQuotedSticker = isQuotedMsg ? (quotedMsg.type === 'stickerMessage') ? true : false : false
+
+        // Mode
+        if (mode === 'self'){
+            if (!fromMe) return
+        }
+        
+        // Anti link
+        if (isGroup && isAntiLink && !isOwner && !isGroupAdmins && isBotGroupAdmins){
+            if (chats.match(/(https:\/\/chat.whatsapp.com)/gi)) {
+                reply(`*「 GROUP LINK DETECTOR 」*\n\nSepertinya kamu mengirimkan link grup, maaf kamu akan di kick`)
+                iqbl.groupRemove(from, [sender])
+            }
+        }
+
+        // Badword
+        if (isGroup && isBadword && !isOwner && !isGroupAdmins){
+            for (let kasar of badword){
+                if (chats.toLowerCase().includes(kasar)){
+                    if (isCountKasar(sender, senbadword)){
+                        if (!isBotGroupAdmins) return reply(`Kamu beruntung karena bot bukan admin`)
+                        reply(`*「 ANTI BADWORD 」*\n\nSepertinya kamu sudah berkata kasar lebih dari 5x, maaf kamu akan di kick`)
+                        iqbl.groupRemove(from, [sender])
+                        delCountKasar(sender, senbadword)
+                    } else {
+                        addCountKasar(sender, senbadword)
+                        reply(`Kamu terdeteksi berkata kasar\nJangan ulangi lagi atau kamu akan dikick`)
+                    }
+                }
+            }
+        }
+
+        // Banned
+        if (isBan) return
+        BannedExpired(ban)
+
+        // MUTE
+        if (isMuted){
+            if (!isGroupAdmins && !isOwner) return
+            if (chats.toLowerCase().startsWith(prefix+'unmute')){
+                let anu = mute.indexOf(from)
+                mute.splice(anu, 1)
+                fs.writeFileSync('./database/mute.json', JSON.stringify(mute))
+                reply(`Bot telah diunmute di group ini`)
+            }
+        }
+        
+        // TicTacToe
+        if (isTicTacToe(from, tictactoe)) tictac(chats, prefix, tictactoe, from, sender, reply, mentions, addBalance, balance)
+
+        // GAME 
+        game.cekWaktuFam(iqbl, family100)
+        game.cekWaktuTG(iqbl, tebakgambar)
+
+        // GAME 
+        if (game.isTebakGambar(from, tebakgambar) && isUser){
+            if (chats.toLowerCase().includes(game.getJawabanTG(from, tebakgambar))){
+                var htgm = randomNomor(100)
+                addBalance(sender, htgm, balance)
+                await reply(`*Selamat jawaban kamu benar*\n*Jawaban :* ${game.getJawabanTG(from, tebakgambar)}\n*Hadiah :* $${htgm}\n\nIngin bermain lagi? kirim *${prefix}tebakgambar*`)
+                tebakgambar.splice(game.getTGPosi(from, tebakgambar), 1)
+            }
+        }
+        if (game.isfam(from, family100) && isUser){
+            var anjuy = game.getjawaban100(from, family100)
+            for (let i of anjuy){
+                if (chats.toLowerCase().includes(i)){
+                    var htgmi = Math.floor(Math.random() * 20) + 1
+                    addBalance(sender, htgmi, balance)
+                    await reply(`*Jawaban benar*\n*Jawaban :* ${i}\n*Hadiah :* $${htgmi}\n*Jawaban yang blum tertebak :* ${anjuy.length - 1}`)
+                    var anug = anjuy.indexOf(i)
+                    anjuy.splice(anug, 1)
+                }
+            }
+            if (anjuy.length < 1){
+                iqbl.sendMessage(from, `Semua jawaban sudah tertebak\nKirim *${prefix}family100* untuk bermain lagi`, text)
+                family100.splice(game.getfamposi(from, family100), 1)
+            }
+        }
+        // Premium
+        _prem.expiredCheck(premium)
+
+        // Auto Regist
+        if (isCmd && !isUser){
+			pendaftar.push(sender)
+			fs.writeFileSync('./database/user.json', JSON.stringify(pendaftar))
+        } 
+        // AFK
+        if (isGroup) {
+            if (mentioned.length !== 0){
+                for (let ment of mentioned) {
+                    if (afk.checkAfkUser(ment, _afk)) {
+                        const getId = afk.getAfkId(ment, _afk)
+                        const getReason = afk.getAfkReason(getId, _afk)
+                        const getTime = Date.now() - afk.getAfkTime(getId, _afk)
+                        const heheh = ms(getTime)
+                        await mentions(`@${ment.split('@')[0]} sedang afk\n\n*Alasan :* ${getReason}\n*Sejak :* ${heheh.hours} Jam, ${heheh.minutes} Menit, ${heheh.seconds} Detik lalu`, [ment], true)
+                        sendMess(ment, `Ada yang mencari anda saat anda offline\n\nNama : ${pushname}\nNomor : wa.me/${sender.split("@")[0]}\nIn Group : ${groupName}\nPesan : ${chats}`)
+                    }
+                }
+            }
+            if (afk.checkAfkUser(sender, _afk)) {
+                _afk.splice(afk.getAfkPosition(sender, _afk), 1)
+                fs.writeFileSync('./database/afk.json', JSON.stringify(_afk))
+                await mentions(`@${sender.split('@')[0]} telah kembali`, [sender], true)
+            }
+        }
+        
+               // Auto Read
+        iqbl.chatRead(from, "read")
+
+        // CMD
+        if (isCmd && !isGroup) {
+			//iqbl.updatePresence(from, Presence.composing)
+            addBalance(sender, randomNomor(20), balance)
+			console.log(color('[CMD]'), color(moment(msg.messageTimestamp * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`))
+        }
+        if (isCmd && isGroup) {
+			//iqbl.updatePresence(from, Presence.composing)
+            addBalance(sender, randomNomor(20), balance)
+			console.log(color('[CMD]'), color(moment(msg.messageTimestamp * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(groupName))
+        }
+
+        if (isOwner){
+            if (chats.startsWith("> ")){
+                console.log(color('[EVAL]'), color(moment(msg.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`Dari Owner aowkoakwoak`))
+                try {
+                    let evaled = await eval(chats.slice(2))
+                    if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+                    textImg(`${evaled}`)
+                } catch (err) {
+                    textImg(`${err}`)
+                }
+            } else if (chats.startsWith("$ ")){
+                console.log(color('[EXEC]'), color(moment(msg.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`Dari Owner aowkoakwoak`))
+                exec(chats.slice(2), (err, stdout) => {
+					if (err) return textImg(`${err}`)
+					if (stdout) textImg(`${stdout}`)
+				})
+            }
+        }
+        
+        switch(command){
+            case 'prefix': case 'cekprefix':{
+                textImg(`${prefix}`)
+            }
+                break
+                }
+    } catch (err) {
+        console.log(color('[ERROR]', 'red'), err)
+    }
+}
